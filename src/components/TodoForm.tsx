3a6e9377133c8a5cb399c4bc3593772c7f5cea2f
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-10 18:02:11 +0300

    Todo App
    * The user can now add to-do lists (but so far there is no component responsible for displaying todo on the user's screen).
    
    * Fixed a problem with the manifest.

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 interface TodoFormProps {
     addTodo: AddTodo;
@@ -7,13 +7,26 @@ interface TodoFormProps {
 export const TodoForm: React.FC<TodoFormProps> = ({addTodo}) => {
     const [newTodo, setNewTodo] = useState<string>('');
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value);
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        addTodo(newTodo);
+        setNewTodo('');
+    }
+
     return (
         <form className="todo-form">
             <input
                 type="text"
+                onChange={handleChange}
                 value={newTodo} 
                 className="todo-input" 
-                placeholder="Add a todo" />
+                placeholder="Add a todo" 
+            />
+            <button type="submit" className="todo-button"> Add Todo </button>
         </form>
     )
 }
